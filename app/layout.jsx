4c7357ff2172642785d977e2e90c8d37b972d8ec
Boss Codes <jadsaderkarate@gmail.com>
2024-01-19 14:36:40 +0200

    Added google ads

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 import Nav from '@components/Nav'
 import Provider from '@components/Provider'
@@ -10,6 +11,10 @@ export const metadata = {
 const RootLayout = ({children}) => {
   return (
     <html lang="en">
+        <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1814557852881278"
+     crossorigin="anonymous"></script>
+        </Head>
         <body>
     <Provider>
       
