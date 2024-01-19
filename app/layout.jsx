86b9b2a6feb16c3dcabb6b85b5981a3bc732a0fd
Boss Codes <jadsaderkarate@gmail.com>
2024-01-19 15:15:51 +0200

    Script tag fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 
 import '../styles/globals.css'
 import Nav from '@components/Nav'
@@ -14,16 +15,18 @@ const RootLayout = ({children}) => {
     <html lang="en">
         <head>
         <meta name="google-adsense-account" content="ca-pub-1814557852881278" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1814557852881278"
-     crossorigin="anonymous"></script>
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-3CY9XEL1Q8"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1814557852881278"
+     crossorigin="anonymous"></Script>
+     <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3CY9XEL1Q8"></Script>
+     <Script strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(&apos;arguments&apos;)}
+    gtag(&apos;js&apos;, new Date());
 
-  gtag('config', 'G-3CY9XEL1Q8');
-</script>
+    gtag(&apos;config&apos;, &apos;G-3CY9XEL1Q8&apos;);
+  `}
+</Script>
         </head>
         <body>
     <Provider>
