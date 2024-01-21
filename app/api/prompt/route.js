3fdc82127ebe7ba9a48b52233aa83838fa50f51e
Boss Codes <jadsaderkarate@gmail.com>
2024-01-21 11:55:44 +0200

    Removed Caching

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,3 @@
-import { connectToDB } from '@utils/database';
-import Prompt from '@models/prompt';
-
 export const GET = async (request) => {
     try{
         await connectToDB();
@@ -8,7 +5,11 @@ export const GET = async (request) => {
         const prompts = await Prompt.find({}).populate('creator');
 
         return new Response(JSON.stringify(prompts), {
-            status: 200
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-store'
+            }
         })
     } catch(error){
         return new Response("Failed to fetch all prompts", { status: 500 }) 
--- next.config.js
+++ next.config.js
@@ -13,7 +13,20 @@ const nextConfig = {
         topLevelAwait: true,
       }
       return config
-    }
-  }
+    },
+    async headers() {
+        return [
+          {
+            source: '/api/prompt',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'no-store, max-age=0',
+              },
+            ],
+          },
+        ];
+    },
+}
   
-  module.exports = nextConfig
+module.exports = nextConfig
